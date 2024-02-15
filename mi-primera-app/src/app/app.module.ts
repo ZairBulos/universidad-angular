58e24707f800b0c72c529884797dc364f2d7be03
ZairBulos <buloszair@gmail.com>
2024-02-15 15:08:52 -0300

    03. Fundamentos de Angular

--- mi-primera-app/src/app/app.module.ts
+++ mi-primera-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mi-primera-app/src/app/persona/persona.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrl: './persona.component.css'
+})
+export class PersonaComponent { }
